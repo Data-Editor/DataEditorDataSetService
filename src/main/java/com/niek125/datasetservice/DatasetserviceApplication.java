6f56b426a9f8b4d153c4ec13d1eec02b5d973360
Niek125 <niek.sleddens@gmail.com>
2019-12-29 21:31:02 +0100

    initial commit

---
+++ src/main/java/com/niek125/datasetservice/DatasetserviceApplication.java
@@ -0,0 +1,13 @@
+package com.niek125.datasetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasetserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niek125/datasetservice/DatasetserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niek125.datasetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasetserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
